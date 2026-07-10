f7ab340360e678df623a279f514cebf14a68fcc8
rajaprof <maharaja.prof@gmail.com>
2026-07-10 13:53:33 +0530

    Deploy navbar active state fix

---
+++ _next/static/chunks/app/page-1c25ed3445b05767.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{1624:(e,s,n)=>{Promise.resolve().then(n.bind(n,768)),Promise.resolve().then(n.t.bind(n,8898,23)),Promise.resolve().then(n.t.bind(n,9151,23))}},e=>{e.O(0,[683,768,480,920,358],()=>e(e.s=1624)),_N_E=e.O()}]);
